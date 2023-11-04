f96d010ef064b22697fc5666415f7bc4f23d4722
Willian Haas <14035698+haaswill@users.noreply.github.com>
2023-11-04 14:07:39 +0100

    - learned how to load gltf model

--- udemy-courses/beginners-guide/main.js
+++ udemy-courses/beginners-guide/main.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+
+import { LoadingBar } from './LoadingBar.js';
 
 class Main {
   constructor() {
@@ -27,75 +31,73 @@ class Main {
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.outputColorSpace = THREE.SRGBColorSpace;
     container.appendChild(this.renderer.domElement);
+    this.setEnvironment();
+
+    this.loadingBar = new LoadingBar();
+    this.loadGLTF();
 
     this.renderer.setAnimationLoop(this.render.bind(this));
-
-    const geometry = this.createStarGeometry();
-    // const geometry = this.createPolygonGeometry();
-    // const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    // const material = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-    // const material = new THREE.MeshPhongMaterial({
-    //   color: 0xff0000,
-    //   specular: 0x444444,
-    //   shininess: 60,
-    // });
-    const material = new THREE.MeshStandardMaterial({
-      color: 0xff0000,
-      wireframe: true,
-    });
-    this.mesh = new THREE.Mesh(geometry, material);
-    this.scene.add(this.mesh);
 
     const controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     window.addEventListener('resize', this.resize.bind(this));
   }
 
-  createStarGeometry(innerRadius = 0.4, outerRadius = 0.8, points = 5) {
-    const shape = new THREE.Shape();
+  setEnvironment() {
+    const loader = new RGBELoader();
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+    pmremGenerator.compileEquirectangularShader();
 
-    const PI2 = Math.PI * 2;
-    const inc = PI2 / (points * 2);
+    const self = this;
 
-    shape.moveTo(outerRadius, 0);
-    let inner = true;
+    loader.load(
+      'textures/venice_sunset_1k.hdr',
+      function (texture) {
+        const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+        pmremGenerator.dispose();
 
-    for (let theta = inc; theta < PI2; theta += inc) {
-      const radius = inner ? innerRadius : outerRadius;
-      shape.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius);
-      inner = !inner;
-    }
-
-    const extrudeSettings = {
-      steps: 1,
-      depth: 1,
-      bevelEnabled: false,
-    };
-
-    return new THREE.ExtrudeGeometry(shape, extrudeSettings);
+        self.scene.environment = envMap;
+      },
+      undefined,
+      function (err) {
+        console.error('An error occurred setting the environment');
+      }
+    );
   }
 
-  createPolygonGeometry(radius = 1, sides = 6) {
-    const shape = new THREE.Shape();
+  loadGLTF() {
+    const loader = new GLTFLoader().setPath('glb/');
 
-    const PI2 = Math.PI * 2;
-    const inc = PI2 / sides;
+    loader.load(
+      'plane.glb',
+      (gltf) => {
+        this.scene.add(gltf.scene);
 
-    shape.moveTo(radius, 0);
-    let inner = true;
+        const bbox = new THREE.Box3().setFromObject(gltf.scene);
 
-    for (let theta = inc; theta < PI2; theta += inc) {
-      shape.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius);
-    }
+        // Get the size of the bounding box to set the scale of the model
+        console.log(
+          `min: ${bbox.min.x.toFixed(2)}, ${bbox.min.y.toFixed(
+            2
+          )}, ${bbox.min.z.toFixed(2)} max: ${bbox.max.x.toFixed(
+            2
+          )}, ${bbox.max.y.toFixed(2)}, ${bbox.max.z.toFixed(2)}`
+        );
+        this.loadingBar.visible = false;
 
-    const extrudeSettings = {
-      steps: 1,
-      depth: radius * 0.25,
-      bevelEnabled: false,
-    };
+        this.renderer.setAnimationLoop(this.render.bind(this));
 
-    return new THREE.ExtrudeGeometry(shape, extrudeSettings);
+        this.plane = gltf.scene;
+      },
+      (xhr) => {
+        this.loadingBar.progress = xhr.loaded / xhr.total;
+      },
+      (err) => {
+        console.error('An error happened loading the GLTF file', err);
+      }
+    );
   }
 
   resize() {
@@ -105,7 +107,9 @@ class Main {
   }
 
   render() {
-    this.mesh.rotateX(0.01);
+    if (this.plane) {
+      this.plane.rotation.y += 0.01;
+    }
     this.renderer.render(this.scene, this.camera);
   }
 }
